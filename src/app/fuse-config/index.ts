69b01cf7958b3a4af421024933537fe23068484f
Escritorio <Escritorio@MACBOOK>
2021-06-22 22:24:02 -0300

    REF

--- src/app/fuse-config/index.ts
+++ src/app/fuse-config/index.ts
@@ -1,16 +1,8 @@
 import { FuseConfig } from '@fuse/types';
 
-/**
- * Default Fuse Configuration
- *
- * You can edit these options to change the default options. All these options also can be
- * changed per component basis. See `app/main/pages/authentication/login/login.component.ts`
- * constructor method to learn more about changing these options per component basis.
- */
-
 export const fuseConfig: FuseConfig = {
-    // Color themes can be defined in src/app/app.theme.scss
-    colorTheme      : 'theme-default',
+    // Alterar Tema
+    colorTheme      : 'theme-blue-gray-dark',
     customScrollbars: true,
     layout          : {
         style    : 'vertical-layout-1',
